90248dd16da56d14a17342e42f166ef9cedcb19d
rkedida <rkedida@1-D-3.42heilbronn.de>
2023-05-30 17:48:56 +0200

    more reading

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -4,6 +4,19 @@
 mysql_install_db
 
 /etc/init.d/mysql start
+
+# Set a root password (replace 'mysecretpassword' with the actual password you want to use)
+mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('Pass') WHERE User='root';"
+
+# Remove anonymous users
+mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+
+# Remove test database
+mysql -u root -e "DROP DATABASE test;"
+
+# Reload the privilege tables
+mysql -u root -e "FLUSH PRIVILEGES;"
+
 # Create your database
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
@@ -12,8 +25,6 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'username'@'%' IDENTIFI
 
 # Flush the privileges
 mysql -u root -e "FLUSH PRIVILEGES;"
-
-# /etc/init.d/mysql stop
 
 # Keep MariaDB running in the foreground
 exec /usr/sbin/mysqld --user=root
