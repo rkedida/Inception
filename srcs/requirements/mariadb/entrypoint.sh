55c382edc9110711b528d45f2f92af6b96b40cc6
PoroCoin <PoroCoin@users.noreply.github.com>
2023-06-02 17:32:25 +0200

    mariaDB works

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 # Start MariaDB service
-mysql_install_db
+mysqld_safe --datadir='/var/lib/mysql' &
 
-/etc/init.d/mysql start
+# Wait for MariaDB service to start
+until mysqladmin ping >/dev/null 2>&1; do
+  echo -n "."; sleep 1;
+done
+
+# Set a root password (replace 'mysecretpassword' with the actual password you want to use)
+mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('pass') WHERE User='root';"
+
+# Remove anonymous users
+mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+
+# Remove test database
+mysql -u root -e "DROP DATABASE IF EXISTS test;"
+
+# Reload the privilege tables
+mysql -u root -e "FLUSH PRIVILEGES;"
+
 # Create your database
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS XXX;"
 
 # Grant all privileges on your database to your user
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'username'@'%' IDENTIFIED BY 'password';"
@@ -13,7 +29,5 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'username'@'%' IDENTIFI
 # Flush the privileges
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-# /etc/init.d/mysql stop
-
 # Keep MariaDB running in the foreground
-exec /usr/sbin/mysqld --user=root
+wait
