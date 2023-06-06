258a821595e3000275a98950ac0e2f374a1771a7
rkedida <rkedida@1-D-3.42heilbronn.de>
2023-06-06 20:15:01 +0200

    test on virtual box

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Start MariaDB service
-mysqld_safe --datadir='/var/lib/mysql' &
+mysql_install_db
+
+service mysql start
 
 # Wait for MariaDB service to start
-until mysqladmin ping >/dev/null 2>&1; do
-  echo -n "."; sleep 1;
-done
+# until mysqladmin ping >/dev/null 2>&1; do
+#   echo -n "."; sleep 1;
+# done
 
 # Set a root password (replace 'mysecretpassword' with the actual password you want to use)
-mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('pass') WHERE User='root';"
+# mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('$MY_SQL_PW') WHERE User='root';"
 
 # Remove anonymous users
 mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
@@ -21,14 +23,16 @@ mysql -u root -e "DROP DATABASE IF EXISTS test;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
 # Create your database
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS XXX;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MY_SQL_DB;"
 
 # Grant all privileges on your database to your user
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'username'@'%' IDENTIFIED BY 'password';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $MY_SQL_DB.* TO '$WP_DB_USER'@'%' IDENTIFIED BY '$MY_SQL_PW';"
 
 # Flush the privileges
 mysql -u root -e "FLUSH PRIVILEGES;"
 
+sleep 30
+service mysql stop
+
 # Keep MariaDB running in the foreground
-# wait
-fg
+exec $@
--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -7,9 +7,11 @@
 # 	sleep 2
 # done
 
+sleep 30
+
 # Proceed with WordPress configuration
-mkdir -p /var/www/html/
-cd /var/www/html/
+mkdir -p /var/www/html/wordpress
+cd /var/www/html/wordpress
 
 if [ -f "/var/www/html/wordpress/wp-config.php" ]
 then
@@ -18,16 +20,17 @@ else
 	# Download and setup WordPress
 	wget -nc https://wordpress.org/latest.zip
 	unzip -o latest.zip && rm latest.zip
-	# mv wordpress/ /var/www/html/
 	# Remove the next line; it won't work inside the container
 	# mv /Users/robelkedida/Documents/Inception/srcs/requirements/ .
 	chown -R www-data:www-data wordpress
+	mv wordpress/* .
+	rm -rf wordpress/
 
 	# Configure WordPress using wp-cli
-	wp --allow-root config create --dbhost=mariadb --dbname=$WP_DB --dbuser=$WP_USER --dbpass=$WP_PASS --locale=en_US 
+	wp --allow-root config create --dbhost=mariadb --dbname=$MY_SQL_DB --dbuser=$WP_DB_USER --dbpass=$WP_PASS --locale=en_US 
 	# Replace '<your-site-url>' with your actual site URL
-	wp --allow-root core install --url=rkedid.42.fr --title=NOT_A_WEBSITE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
-	new_user_id=$(wp --allow-root user create test test@test.com --user_pass=secret --porcelain)
+	wp --allow-root core install --url=rkedida.42.fr --title=NOT_A_WEBSITE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
+	# new_user_id=$(wp --allow-root user create test test@test.com --user_pass=secret --porcelain)
 	echo "New user created with ID: $new_user_id"
 fi
 
