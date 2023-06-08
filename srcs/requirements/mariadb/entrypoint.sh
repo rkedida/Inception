d21dd8b5598f0881c75b5c42929fd66c41011b66
rkedida <rkedida@1-D-3.42heilbronn.de>
2023-06-08 22:20:33 +0200

    mandatory done

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 
 # Start MariaDB service
-mysql_install_db
-
 service mysql start
 
 # Wait for MariaDB service to start
-# until mysqladmin ping >/dev/null 2>&1; do
-#   echo -n "."; sleep 1;
-# done
+until mysqladmin ping >/dev/null 2>&1; do
+  echo -n "."; sleep 1;
+done
 
-# Set a root password (replace 'mysecretpassword' with the actual password you want to use)
-# mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('$MY_SQL_PW') WHERE User='root';"
+# Run mysql_secure_installation
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$MY_SQL_PW\r\"
+expect \"Set root password?\"
+send \"Y\r\"
+expect \"New password:\"
+send \"$MY_SQL_PW\r\"
+expect \"Re-enter new password:\"
+send \"$MY_SQL_PW\r\"
+expect \"Remove anonymous users?\"
+send \"Y\r\"
+expect \"Disallow root login remotely?\"
+send \"Y\r\"
+expect \"Remove test database and access to it?\"
+send \"Y\r\"
+expect \"Reload privilege tables now?\"
+send \"Y\r\"
+expect eof
+")
 
-# Remove anonymous users
-mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
-
-# Remove test database
-mysql -u root -e "DROP DATABASE IF EXISTS test;"
-
-# Reload the privilege tables
-mysql -u root -e "FLUSH PRIVILEGES;"
+echo "$SECURE_MYSQL"
 
 # Create your database
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MY_SQL_DB;"
+mysql -u root -p$MY_SQL_PW -e "CREATE DATABASE IF NOT EXISTS $MY_SQL_DB;"
 
 # Grant all privileges on your database to your user
-mysql -u root -e "GRANT ALL PRIVILEGES ON $MY_SQL_DB.* TO '$WP_DB_USER'@'%' IDENTIFIED BY '$MY_SQL_PW';"
+mysql -u root -p$MY_SQL_PW -e "GRANT ALL PRIVILEGES ON $MY_SQL_DB.* TO '$WP_DB_USER'@'%' IDENTIFIED BY '$MY_SQL_PW';"
 
-# Flush the privileges
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-sleep 30
 service mysql stop
 
 # Keep MariaDB running in the foreground
-exec $@
+exec "$@"
--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Wait until the database is accessible
-# until mysqladmin ping -h"$WP_DB_HOST" -u"$WP_USER" -p"$WP_PASS"
-# do
-# 	echo "$(date): waiting for MariaDB to be ready..."
-# 	sleep 2
-# done
-
-sleep 30
+sleep 60
 
 # Proceed with WordPress configuration
 mkdir -p /var/www/html/wordpress
@@ -20,18 +13,13 @@ else
 	# Download and setup WordPress
 	wget -nc https://wordpress.org/latest.zip
 	unzip -o latest.zip && rm latest.zip
-	# Remove the next line; it won't work inside the container
-	# mv /Users/robelkedida/Documents/Inception/srcs/requirements/ .
 	chown -R www-data:www-data wordpress
 	mv wordpress/* .
 	rm -rf wordpress/
-
 	# Configure WordPress using wp-cli
 	wp --allow-root config create --dbhost=mariadb --dbname=$MY_SQL_DB --dbuser=$WP_DB_USER --dbpass=$WP_PASS --locale=en_US 
 	# Replace '<your-site-url>' with your actual site URL
 	wp --allow-root core install --url=rkedida.42.fr --title=NOT_A_WEBSITE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
-	# new_user_id=$(wp --allow-root user create test test@test.com --user_pass=secret --porcelain)
-	echo "New user created with ID: $new_user_id"
 fi
 
 # Start PHP-FPM
